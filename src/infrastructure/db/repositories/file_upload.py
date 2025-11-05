b5338319a5bb47162ff88c5bd7f9c35c08fa9643
KarGalan <dondishmen@gmail.com>
2025-11-05 11:58:38 +0500

    feat/added error printing and rollback

--- src/infrastructure/db/repositories/file_upload.py
+++ src/infrastructure/db/repositories/file_upload.py
@@ -10,16 +10,21 @@ class FileUploadRepository:
 
     # CREATE 
     async def create(self, path: str, size_bytes: int, mime: str, owner_user_id: int) -> FileUpload:
-        new_file = FileUpload(
-            path=path,
-            size_bytes=size_bytes,
-            mime=mime,
-            owner_user_id=owner_user_id
-        )
-        self.session.add(new_file)
-        await self.session.commit()
-        await self.session.refresh(new_file)
-        return new_file
+        try:
+            new_file = FileUpload(
+                path=path,
+                size_bytes=size_bytes,
+                mime=mime,
+                owner_user_id=owner_user_id
+            )
+            self.session.add(new_file)
+            await self.session.commit()
+            await self.session.refresh(new_file)
+            return new_file
+        except Exception as e:
+                await self.session.rollback()
+                print(f"[ERROR][CREATE] {e}")
+                return None
 
     # READ  по id
     async def get_by_id(self, file_id: int) -> FileUpload | None:
@@ -37,18 +42,24 @@ class FileUploadRepository:
 
     # UPDATE путь | размер
     async def update(self, file_id: int, path: str | None = None, size_bytes: int | None = None) -> FileUpload | None:
-        file = await self.get_by_id(file_id)
-        if not file:
+        try:
+            file = await self.get_by_id(file_id)
+            if not file:
+                print(f"<<[WARN][UPDATE]>> File with id={file_id} not found")
+                return None
+
+            if path is not None:
+                file.path = path
+            if size_bytes is not None:
+                file.size_bytes = size_bytes
+
+            await self.session.commit()
+            await self.session.refresh(file)
+            return file
+        except Exception as e:
+            await self.session.rollback()
+            print(f"<<[ERROR][UPDATE]>> {e}")
             return None
-
-        if path is not None:
-            file.path = path
-        if size_bytes is not None:
-            file.size_bytes = size_bytes
-
-        await self.session.commit()
-        await self.session.refresh(file)
-        return file
 
     # DELETE 
     async def delete(self, file_id: int) -> bool:
