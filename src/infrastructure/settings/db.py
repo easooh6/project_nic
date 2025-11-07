a03f8b8267c5deba76863d424e61c58b8982c097
Tarlimo <118112145+Tarlimo@users.noreply.github.com>
2025-11-07 18:45:02 +0500

    Add DBSettings class for database configuration

---
+++ src/infrastructure/settings/db.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class DBSettings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def DATABASE_URL_asyncpg(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    class Config:
+        env_file = '.env'
+        extra = 'ignore'
