4970b4cb8cca82bd5eddcc2e02f16033d19c8d4b
Гирский Андрей <andreygirskiy291@gmail.com>
2025-11-08 15:19:04 +0500

    ended cruds

--- src/infrastructure/db/repositories/booking.py
+++ src/infrastructure/db/repositories/booking.py
@@ -5,22 +5,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, delete, update
 from typing import Callable, AsyncContextManager
 from datetime import datetime, timedelta
+from src.domain.enums.booking_status import BookingStatus
 
 class BookingRepository:
 
     def __init__(self):
         self.session_factory: Callable[[], AsyncContextManager[AsyncSession]] = get_db_session
     
-    async def create_booking(self, entity: Booking) -> Booking | None:
+    async def create_booking(self, entity: Booking) -> Booking:
         
         async with self.session_factory() as session:
-
-            query_check = select(BookingModel).where(BookingModel.user_id==entity.user_id,
-                                                     BookingModel.created_at==entity.created_at
-                                                     )
-            check_result = (await session.execute(query_check)).scalar_one_or_none()
-            if check_result:
-                return None
 
             booking_model = BookingModel(**entity.model_dump())
             session.add(booking_model)
@@ -28,7 +22,9 @@ class BookingRepository:
             await session.flush()
             await session.refresh(booking_model)
 
-            return entity.model_validate(booking_model)
+            entity = Booking.model_validate(booking_model)
+
+            return entity
     
     async def get_booking_by_id(self, id: int) -> Booking | None:
 
@@ -65,7 +61,27 @@ class BookingRepository:
             if not models_raw:
                 return None
 
-            return [Booking.model_validate(model) for model in models_raw]    
+            models = [Booking.model_validate(model) for model in models_raw]
+
+            return models
+    
+    async def get_booking_by_status(self, user_id: int, status: BookingStatus) -> list[Booking] | None:
+
+        async with self.session_factory() as session:
+
+            query = select(BookingModel).where(
+                BookingModel.user_id==user_id,
+                BookingModel.status==status
+                )
+            
+            models_raw = (await session.execute(query)).scalars().all()
+
+            if not models_raw:
+                return None
+            
+            models = [Booking.model_validate(model) for model in models_raw]
+
+            return models
         
     async def get_booking_by_user_id(self, user_id: int) -> list[Booking] | None:
 
@@ -81,12 +97,12 @@ class BookingRepository:
 
             return models   
         
-    async def update_booking_status(self, booking: Booking) -> int:
+    async def update_booking_status(self, entity: Booking) -> int:
 
         async with self.session_factory() as session:
 
-            query = update(BookingModel).where(BookingModel.id == booking.id).values(
-                    status=booking.status
+            query = update(BookingModel).where(BookingModel.id == entity.id).values(
+                    status=entity.status
                 )
             result = await session.execute(query)
  
