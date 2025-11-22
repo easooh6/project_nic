c0c870173c888eca7380acbd09bce0746b082e2a
Гирский Андрей <andreygirskiy291@gmail.com>
2025-11-22 22:02:13 +0500

    Feature/redis (#32)
    * added router /me, role verification, access verification
    
    * new service
    
    * fixed verification
    
    * added /refresh endpoint, its service part
    
    * verified dev version
    
    * small fix
    
    * fix
    
    * added redis connection, changed main.py
    
    * added redis settings, client

--- src/infrastructure/settings/settings.py
+++ src/infrastructure/settings/settings.py
@@ -1,8 +1,11 @@
 from src.infrastructure.settings.db import DBSettings
 from src.infrastructure.settings.auth import AuthSettings
+from src.infrastructure.settings.redis import RedisSettings
+
 class Settings:
     def __init__(self):
         self.db = DBSettings()
         self.auth = AuthSettings()
-
+        self.redis = RedisSettings()
+        
 settings = Settings()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from src.presentation.routers.auth.auth import router as auth_router
+from contextlib import asynccontextmanager
+from src.infrastructure.redis.client import init_redis_client, close_redis_client
+from src.infrastructure.db.db import init_db, close_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    await init_redis_client()
+    yield
+    await close_db()
+    await close_redis_client()
 
 app = FastAPI(
+    lifespan=lifespan,
     title="NIC project",
     description="API for NIC project",
     version="0.1.0"
 )
 
+    
 # пример подключения роутеров
 # app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 # app.include_router(user_router, prefix="/user", tags=["User Profile"])
