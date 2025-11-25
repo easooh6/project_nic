ddf7999b36d3dfa59f9af304bbcb455dbd7abb2f
KarGalan <dondishmen@gmail.com>
2025-11-25 23:15:56 +0500

    feat/replaced service and func was used

--- src/domain/services/email_service.py
+++ src/domain/services/email_service.py
@@ -1,30 +1,9 @@
-import aiosmtplib
-from email.message import EmailMessage
-from src.infrastructure.settings.settings import settings
+from src.infrastructure.email import EmailService
 
-class EmailService:
-    def __init__(self):
-        self.host = settings.email.SMTP_HOST
-        self.port = settings.email.SMTP_PORT
-        self.username = settings.email.SMTP_USER
-        self.password = settings.email.SMTP_PASSWORD
-        self.email_from = settings.email.EMAIL_FROM
-        self.use_tls = settings.email.EMAIL_TLS
+email_service = EmailService()
+async def send_email(to: str, subject: str, body: str):
+    return await email_service.send_email(to, subject, body) 
 
-    async def send_email(self, to: str, subject: str, body: str):
-        message = EmailMessage()
-        message["From"] = self.email_from
-        message["To"] = to
-        message["Subject"] = subject
-        message.set_content(body)
-
-        await aiosmtplib.send(
-            message,
-            hostname=self.host,
-            port=self.port,
-            username=self.username,
-            password=self.password,
-            start_tls=self.use_tls,
-        )
-
+async def verify_code(code: str) -> bool:
+    if code == "corect": #заглушка
         return True
