48645f6b2ff4add434a65b3b9d32b0bf67b40667
Гирский Андрей <andreygirskiy291@gmail.com>
2025-12-11 21:07:28 +0500

    final push

---
+++ src/tasks/worker_imports.py
@@ -0,0 +1,6 @@
+
+from src.tasks.scheduler.upload_task import cleanup_uploads_task
+from src.tasks.scheduler.hold_task import release_holds_task
+from src.tasks.scheduler.booking_task import archive_bookings_task
+
+from src.tasks.broker import broker
